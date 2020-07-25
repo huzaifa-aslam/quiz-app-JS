beb72588d80ffa62a1d00ff2a0d7168727d9f093
huzaifa-aslam <56935684+huzaifa-aslam@users.noreply.github.com>
2020-07-25 13:05:12 -0700

    Add files via upload
    updated

--- app.js
+++ app.js
@@ -37,70 +37,152 @@ var myQuestions = [
 
         correctAnser: 2
 
+    },
+    {
+        question: "1+1=",
+        answer: {
+            a: 3,
+            b:6,
+            c: 0,
+            d:"two"
+
+        },
+
+        correctAnser: "two"
+
     }
 
 ]
 
 
+var i = 0;
+//var txt = 'Lorem ipsum dummy text blabla.';
+var speed = 50;
+
+let userInfo=document.getElementById('form')
+
 let userScore=document.getElementById('myScore');
 
 let quesData = document.getElementById("question");
 
-let quesOption=document.getElementById("option");
+let userName=document.getElementById('username')
+
+let quesOption=document.getElementById("option")
+
+let nextSpan=document.getElementById("spanNext")
+
+let nextButton=document.getElementById("nextBtn")
+
 let startQuiz=document.getElementById("start");
 let nextQues=document.getElementById("next");
+nextQues.disabled=true
+nextButton.style.visibility = "hidden"
 nextQues.style.visibility = "hidden"
+userScore.style.visibility = "hidden"
+
 let ques=0
+let abc=ques
 let score=0
-let userInput=''
+let userInput=[]
+let quesInc=1
+let copied=[];
+let finalCopied=[]
+let corrAnswer=[]
 
-function getValue(event){
-    userInput=event.target.value
-    if(userInput==myQuestions[ques].correctAnser){
-        score++
-       
+let state={
 
+}
+
+
+
+const  typeWriter=(txt)=> {
+
+    if (i < txt.length) {
+      document.getElementById("myScore").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
     }
-    ques++
-    console.log(userInput)
-    console.log(score)
+  }
+
+ 
+
+  function getValue(event){
+       
+   
+        
+   
+    userInput.push(event.target.value)
+    copied=userInput.slice(-1)
+   
+     
+    
+    
+}
+
+function submitAnswer(){
+    nextQues.disabled=false
+    finalCopied.push(copied)
+   
+       
+    
+    console.log("finalCopied",finalCopied)
 }
 
 function start(){
+   nextButton.style.visibility=""
     nextQues.style.visibility = ""
     startQuiz.style.visibility = "hidden"
-    next()
+    userInfo.style.visibility = "hidden"
+   next()
 
 }
 
 
+
 function next(){
-  
+ 
+    nextQues.disabled=true
+       
+        
     if(ques==myQuestions.length){
-        myScore.innerHTML=`Your Score is ${score} out of ${myQuestions.length}`
+        userScore.style.visibility = ""
+        nextQues.disabled=true
+        for(var k=0;k<=myQuestions.length-1;k++){
+            if(corrAnswer[k]==finalCopied[k]){
+                score++
+            }
+        }
+        nextSpan.innerHTML="Finished"
+        userScore.classList.add("alert-success")
+
+        myScore.innerHTML=`${userName.value} Your Score is ${score} out of ${myQuestions.length}`
     return false
     }
-
+  
     quesData.innerHTML=""
     quesOption.innerHTML=""
 
-
+  
 
 // creat question
 
 let h3=document.createElement("h3")
-    let quesText=document.createTextNode(myQuestions[ques].question)
+    let quesText=document.createTextNode("Q"+quesInc +".) "+" "+myQuestions[ques].question)
     h3.appendChild(quesText)
     quesData.appendChild(h3)
-
+   
+    corrAnswer.push(myQuestions[ques].correctAnser)
 
     let check=myQuestions[ques].answer
+  
+
     for (const property in check) {
  
 // creat radio button 
 let radioInput=document.createElement("input")
 radioInput.setAttribute("type","radio")
 radioInput.setAttribute("name","option")
+radioInput.setAttribute("class","choose")
 radioInput.setAttribute("required","required")
 radioInput.setAttribute("onclick","getValue(event)")
 radioInput.setAttribute("value",`${check[property]}`)
@@ -115,22 +197,22 @@ label.appendChild(labelText)
 quesOption.appendChild(label)
 
 let br=document.createElement("br")
-label.appendChild(br)
+quesOption.appendChild(br)
        
       }
-     // console.log(myQuestions[ques].correctAnser)
-  
-    
-    
-
-    
-
+    //   console.log("ques",myQuestions[ques])
+    //  console.log("ans",corrAnswer)
+      
    
+     quesInc++
+   ques++
+    
+     
+    
       
 }
 
-
-
+//console.log("ans2",userInput)
 
 
 
